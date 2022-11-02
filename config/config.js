5a882532a7205e9b8ef65e87c4fbb15053dc6acc
echeverriaalex <aleex.naahuel@outlook.com>
2022-11-02 15:59:53 -0300

    amoldo ami configuracion

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     "username": process.env.DATABASE_USER,
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
-    "host": "localhost",
+    "host": "127.0.0.1", // "localhost",
     "dialect": "mysql"
   },
   "test": {
